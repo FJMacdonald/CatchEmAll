77571748f162a5e7ec8385d1e9d598efea1b5393
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-27 03:24:20 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Francesca MACDONALD on 2023-08-27.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
